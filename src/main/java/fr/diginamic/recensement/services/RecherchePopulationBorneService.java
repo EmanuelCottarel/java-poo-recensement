896c221110727ab4d203f08bf30b963d36252a22
EmanuelCottarel <emanuelcottarel@gmail.com>
2024-04-17 17:05:53 +0200

    TP 20

--- src/main/java/fr/diginamic/recensement/services/RecherchePopulationBorneService.java
+++ src/main/java/fr/diginamic/recensement/services/RecherchePopulationBorneService.java
@@ -3,8 +3,11 @@ package fr.diginamic.recensement.services;
 import java.util.List;
 import java.util.Scanner;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 import fr.diginamic.recensement.entites.Recensement;
 import fr.diginamic.recensement.entites.Ville;
+import fr.diginamic.recensement.exceptions.SaisieException;
 
 /**
  * Recherche et affichage de toutes les villes d'un département dont la
@@ -17,21 +20,37 @@ import fr.diginamic.recensement.entites.Ville;
 public class RecherchePopulationBorneService extends MenuService {
 
 	@Override
-	public void traiter(Recensement rec, Scanner scanner) {
+	public void traiter(Recensement rec, Scanner scanner) throws SaisieException {
 
 		System.out.println("Quel est le code du département recherché ? ");
 		String choix = scanner.nextLine();
 
 		System.out.println("Choississez une population minimum (en milliers d'habitants): ");
 		String saisieMin = scanner.nextLine();
-		
+
 		System.out.println("Choississez une population maximum (en milliers d'habitants): ");
 		String saisieMax = scanner.nextLine();
 
+		if (!NumberUtils.isDigits(saisieMin)) {
+			throw new SaisieException("Veuillez saisir un nombre pour le minimum");
+		}
+		if (!NumberUtils.isDigits(saisieMax)) {
+			throw new SaisieException("Veuillez saisir un nombre pour le maximum");
+		}
+
+		if (Integer.parseInt(saisieMin) < 0 || Integer.parseInt(saisieMax) < 0
+				|| Integer.parseInt(saisieMin) > Integer.parseInt(saisieMax)) {
+			throw new SaisieException(
+					"Une des valeur saisie est inférieure à zero ou le minimum est supérieur au maximum.");
+		}
+
 		int min = Integer.parseInt(saisieMin) * 1000;
 		int max = Integer.parseInt(saisieMax) * 1000;
-		
+
 		List<Ville> villes = rec.getVilles();
+		if (villes.stream().filter(ville -> ville.getCodeDepartement().equals(choix)).toArray().length == 0) {
+			throw new SaisieException("Le code département saisi est invalide");
+		}
 		for (Ville ville : villes) {
 			if (ville.getCodeDepartement().equalsIgnoreCase(choix)) {
 				if (ville.getPopulation() >= min && ville.getPopulation() <= max) {
